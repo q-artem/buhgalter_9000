80faedd800ff42f3c016d38409cabc2a7e76cc5d
RofleX <94610518+RofIeX@users.noreply.github.com>
2025-10-01 00:12:47 +0300

    fix env

--- utils/config_reader.py
+++ utils/config_reader.py
@@ -5,6 +5,7 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     bot_token: SecretStr
 
-    model_config = SettingsConfigDict(env_file='../.env', env_file_encoding='utf-8')
+    class Config:
+        env_file = ".env"
 
 config = Settings()
